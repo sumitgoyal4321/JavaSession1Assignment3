abe3edca59efc4b179ae4a2f7a6b42e161b4ef42
sumitgoyal4321 <sumitgoyal4321@gmail.com>
2016-03-03 22:44:34 +0530

    Added files via upload

---
+++ DataTypeExample.java
@@ -0,0 +1,16 @@
+
+public class DataTypeExample {
+	public static void main(String[]str)
+	{
+		int intVar = 10;
+		float floatVar = 10.5f;
+		double doubleVar = 10.0;
+		String strVar = "sumit";
+		char ch = 'c';
+		
+
+		
+		int a = 10.0;
+		int b = 'c';
+	}	
+}
